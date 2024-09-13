502627c506dd7dee31571881cfc057e6bc02d612
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-09-13 15:48:47 +0200

    Add pseudo code for getHumanChoice function

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const options = ["rock", "paper", "scissors"]
+
 function getComputerChoice() {
     return options[Math.floor(Math.random() * options.length)];
 }
+
+// ASK FOR USER INPUT STATING THE AVAILABLE OPTIONS
+// CHECK IF USER INPUT IS A VALID OPTION
+// IF USER INPUT NOT VALID OPTION --> RETURN TRY AGAIN MESSAGE
+// IF USER INPUT VALID OPTION --> RETURN USER INPUT
